5542dfdb95c2a46200a363f9c6cfd0043fc62a4a
Tomaya001 <63055693+Tomaya001@users.noreply.github.com>
2020-05-04 08:27:49 -0300

    Rotaso

---
+++ Assets/_SRC/Scripts/AttacksComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttacksComponent: StatsComponent
+{
+    public void attack1()
+    {
+
+    }
+
+    public void attack2()
+    {
+
+    }
+    public void attack3()
+    {
+
+    }
+}
---
+++ Assets/_SRC/Scripts/Stats.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats
+{
+    int vida;
+    int daño;
+    int armor;
+    public int speed;
+}
