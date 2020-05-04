702e9e0f427bce18d11ec425daaebcc5b28427f1
Tomaya001 <63055693+Tomaya001@users.noreply.github.com>
2020-05-04 07:58:43 -0300

    Primera Carga

---
+++ Assets/_SRC/Scripts/MovementComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementComponent : MonoBehaviour
+{
+    Transform t;
+    /*StatsComponent stats;
+
+    private void Awake()
+    {
+        t = transform;
+        stats = GetComponent<StatsComponent>();
+    }
+
+    public void Move(int dir)
+    {        
+        t.Translate(Vector3.right * dir * stats.speed * Time.deltaTime);
+    }*/
+}
---
+++ Assets/_SRC/Scripts/StatsComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsComponent
+{
+    int vida;
+    int daño;
+    int armor;
+    public int speed;
+}
